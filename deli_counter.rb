ad915f178f3f7cf06e8e7b0400233109acfac8ab
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-25 03:30:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+require 'pry' 
+
+def line(katz_deli)
+
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else
+string_position_name = ""    
+i = 1
+katz_deli.each do |name|
+  string_position_name = "#{string_position_name} #{i.to_s}. #{name}"
+  i+=1
+end
+
+puts "The line is currently:#{string_position_name}"
+
+end 
+
+end
+
+def take_a_number(katz_deli, name)
+  position = katz_deli.size 
+  puts “welcome, #{name}. You are number #{position} in line.”
+
+
